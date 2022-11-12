5debaa4418cae06881fdcf822fd26227c904700c
Ravikant Tyagi <dhruvasharma1989@gmail.com>
2022-11-12 16:54:02 +0530

    Add files via upload

---
+++ loop_1.py
@@ -0,0 +1,38 @@
+#stop = 5
+for i in range(5):
+    print(i)
+
+print("*" * 20)
+
+#start = 1
+#stop = 11 (n-1)
+#step = 1
+for i in range(1,11):
+    print(i)
+
+print("*" * 20)
+
+#start = 2
+#stop = 21 (n-1)
+#step = 2
+for i in range(2,21,2):
+    print(i)
+
+print("*" * 20)
+
+#start = 11
+#stop = 1 (n+1)
+#step = -1
+for i in range(11,1,-1):
+    print(i)
+
+
+
+
+
+
+
+
+
+
+
